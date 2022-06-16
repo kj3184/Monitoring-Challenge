011845d9bbfcee60536146ea43f8b038ca9235ca
kj3184 <kunal.jadhav84@gmail.com>
2022-06-17 04:24:27 +0800

    updated comments in file

--- service_monitor.py
+++ service_monitor.py
@@ -6,8 +6,17 @@ pd.options.mode.chained_assignment = None  # default='warn'
 import numpy as np
 import argparse
 
+# author : Kunal J
+# date 16 June 2022
+# This is monitoring tool that will query Cloud Provider X (CPX) and will fetch the server details with following metrics.
+# A. Fetch all running microservices
+# B. Print average CPU/Memory of services of the same type 
+# c. fetch the services which have fewer than 2 healthy instances running 
+# D. print CPU/Memory of all instances of a given service over a time.
+ 
 
 
+# function to call CPX API for fetching all running servers
 def fetch_all_cpx_servers(port):
     url = "http://localhost:"+port+"/servers"
     # print(url)
@@ -17,6 +26,8 @@ def fetch_all_cpx_servers(port):
     ls = strServerNameList.strip('[]').replace('"', '').replace(' ', '').split(',')
     return ls
 
+
+# This function calls CPX API for fetching individual running server details, it will return JSON string
 def fetch_cpx_server_details(ls, str1,port):
     # print(port)
     for i in range(len(ls)):
@@ -30,7 +41,8 @@ def fetch_cpx_server_details(ls, str1,port):
             str1=str1+","+jsonStr
     return str1
 
-
+# This uses the json string returned in fetch_cpx_server_details function
+# and call CPX API for fetching all running microservices.
 def print_running_services(ls, str1):
     df=pd.read_json(str1,lines=True)
     pd.set_option('display.max_rows', len(ls))
@@ -50,6 +62,7 @@ def print_running_services(ls, str1):
     df2 = df.to_string(index=False)
     print (df2)
 
+# This returns the average cpu and memory usage for each microservice
 def get_avg_cpumem_usage(ls, str1):
     df=pd.read_json(str1,lines=True)
     pd.set_option('display.max_rows', len(ls))
@@ -70,6 +83,8 @@ def get_avg_cpumem_usage(ls, str1):
     df_groupedby['Memory'] = df_groupedby['Memory'].astype(str) + '%'
     print(df_groupedby)
 
+
+# This reports the microservices which have fewer than 2 healthy instances running 
 def topmost_fewer_healthy_instaces(ls, str1):
     df=pd.read_json(str1,lines=True)
     pd.set_option('display.max_rows', len(ls))
@@ -86,6 +101,7 @@ def topmost_fewer_healthy_instaces(ls, str1):
     df = df.reindex(columns=column_names)
     df['CPU'] = df['CPU'].astype(str) + '%'
     df['Memory'] = df['Memory'].astype(str) + '%'
+
     df_filtered=df.query("Status == 'Healthy'")
     df_filtered.drop('CPU', axis=1, inplace=True)
     df_filtered.drop('IP', axis=1, inplace=True)
@@ -95,7 +111,38 @@ def topmost_fewer_healthy_instaces(ls, str1):
     print(fewer_instances)
 
 
-def main(port: str, menuoption: str):
+# This reports the microservices which fetches cpu and memory details for provided service type
+# it will fetch details continously until it gets interrupted by keyboard 
+def print_cpumem_details_service(ls, str1,serviceName=""):
+    while True:
+        try:
+            df=pd.read_json(str1,lines=True)
+            pd.set_option('display.max_rows', len(ls))
+            pd.set_option('display.colheader_justify', 'center')
+            df['cpu'] = pd.to_numeric(df['cpu'].str.replace('%', ''))
+            df['memory'] = pd.to_numeric(df['memory'].str.replace('%', ''))
+
+            df.rename(columns = {'IP':'IP', 'cpu':'CPU', 'memory':'Memory',
+        'service':'Service'},inplace = True)
+            df['Status'] = 'Healthy'
+            df.loc[(df['CPU'] >= 80) & (df['Memory'] >= 80), 'Status'] = 'Unhealthy'
+
+            column_names = ['IP','Service', 'Status','CPU','Memory']
+            df = df.reindex(columns=column_names)
+            df['CPU'] = df['CPU'].astype(str) + '%'
+            df['Memory'] = df['Memory'].astype(str) + '%'
+            # df.loc[df['Service'] == serviceName]
+            df_filtered=df.query("Service ==\""+serviceName+"\"")
+            # print(df_filtered)
+            df2 = df_filtered.to_string(index=False)
+            # df2.query('Service == \"'+serviceName+'\"')
+            print (df_filtered)
+        except KeyboardInterrupt:
+            print("Interrupted")
+            break
+            
+
+def main(port: str, menuoption: str,serviceName = ""):
     ls = fetch_all_cpx_servers(port)
     str1=''
     str1 = fetch_cpx_server_details(ls, str1,port)
@@ -106,10 +153,13 @@ def main(port: str, menuoption: str):
         get_avg_cpumem_usage(ls, str1)
     elif menuoption == '3':
         topmost_fewer_healthy_instaces(ls, str1)
-    
+    elif menuoption =='4':
+        print_cpumem_details_service(ls,str1,serviceName)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("port", help="the port on which to run", type=str)
     parser.add_argument("menuoption", help="the menu option for extraction", type=str)
+    parser.add_argument("serviceName", help="the service name for extraction", type=str)
+
     args = parser.parse_args()
-    main(args.port,args.menuoption)
+    main(args.port,args.menuoption,args.serviceName)
